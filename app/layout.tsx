4e02a2ed2de3db8fd678040cdc0b14e353918c9d
Sankalp Pradhan <sankalppradhan1906@gmail.com>
2026-05-13 17:57:03 +0530

    build errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,11 @@ import {
 
 import "./globals.css";
 
+
 import Navbar from "./home/nav";
 import CTA from "./home/cta";
 import Footer from "./home/footer";
+import Script from "next/script";
 
 const plusJakarta = Plus_Jakarta_Sans({
   variable: "--font-display",
@@ -142,6 +144,27 @@ export default function RootLayout({
 
         <CTA />
         <Footer />
+        {/* Google Analytics */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+          strategy="afterInteractive"
+        />
+
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag(){
+      dataLayer.push(arguments);
+    }
+
+    window.gtag = gtag;
+
+    gtag('js', new Date());
+
+    gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+  `}
+        </Script>
       </body>
     </html>
   );
